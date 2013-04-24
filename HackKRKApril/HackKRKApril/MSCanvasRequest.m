13663fe33cb30ef04db834c1eee2834dd739dd4e
Marek Serafin <marek@snowheads.pl>
2013-04-24 20:58:16 +0200

    Upload

--- HackKRKApril/HackKRKApril/MSCanvasRequest.m
+++ HackKRKApril/HackKRKApril/MSCanvasRequest.m
@@ -7,12 +7,14 @@
 //
 
 static NSString *kApiTokenKey   = @"api_token";
+static NSString *kApiImageKey   = @"image";
 static NSString *kApiToken      = @"synergize sexy action-items";
 static NSString *kApiBaseUrl    = @"http://canvas.hackkrk.com/api/";
 
 #import "MSCanvasRequest.h"
 #import "AFNetworking.h"
 #import "AFJSONRequestOperation.h"
+#import "Base64.h"
 
 @implementation MSCanvasRequest
 
@@ -54,6 +56,42 @@ static NSString *kApiBaseUrl    = @"http://canvas.hackkrk.com/api/";
     }];
     
     [operation start];
+}
+
+- (void)postChallengeWithImage:(NSImage*)image
+                   andServerId:(int)serverId
+{
+
+    NSData *imageData = [self PNGRepresentationOfImage:image];
+    NSString *imageString = [[NSString alloc] initWithBytes: [imageData bytes]
+                                                     length: [imageData length]
+                                                   encoding: NSUTF8StringEncoding];
+    
+    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL: [NSURL URLWithString: kApiBaseUrl]];
+    
+    NSMutableURLRequest *request = [httpClient requestWithMethod: @"POST"
+                                                            path: [NSString stringWithFormat:@"challenge/%d", serverId]
+                                                      parameters: @{ kApiTokenKey: kApiToken, kApiImageKey: [imageString base64EncodedString]}];
+    
+    AFJSONRequestOperation *operation = [[AFJSONRequestOperation alloc] initWithRequest:request];
+    
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSLog(@"%@", responseObject);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@", error);
+    }];
+    
+    [operation start];
+}
+
+- (NSData *) PNGRepresentationOfImage:(NSImage *) image
+{
+    [image lockFocus];
+    NSBitmapImageRep *bitmapRep = [[NSBitmapImageRep alloc] initWithFocusedViewRect:NSMakeRect(0, 0, image.size.width, image.size.height)];
+    [image unlockFocus];
+    return [bitmapRep representationUsingType:NSPNGFileType properties:Nil];
 }
 
 
